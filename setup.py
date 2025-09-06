bd91d352b6002e43c9ebf2df232125dfb03a8c18
sajadirc <sajad.jen@gmail.com>
2025-09-06 10:04:20 +0330

    bug fixed

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 # setup(name= 'printer', version= '1.0.1', description='this Module print same text')
 setup(
     name="printer",
-    version="1.0.3",
+    version="1.0.4",
     description="this Module print same text",
     url="https://github.com/sajadirc/print-package.git",
-    requires= ['request']
+    requires= ['requests']
 )
